8a94872d93a14fed129fd6a7f34cdb6ab14ff8d0
GonzRon <Gonz.Ron@Gmail.com>
2026-09-14 19:37:30 -0400

    link launch policy, notenfc://tag route, save/open link use cases

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.kotlin.test)
+    testImplementation(libs.kotlinx.coroutines.test)
     testRuntimeOnly(libs.junit.platform.launcher)
 }
 
